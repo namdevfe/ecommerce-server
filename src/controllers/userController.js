a1477c7fecfbcccb9f69cc6a01179a30ed7036dc
Nguyễn Kim Quốc Nam <namnkq2603@gmail.com>
2024-04-16 23:17:34 +0700

    feat: handle logic register & login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,9 @@
 import asyncHandler from 'express-async-handler'
-import User from '~/models/user'
+import userService from '~/services/userService'
 
 const register = asyncHandler(async (req, res) => {
-  // Get value register of user from client
   const { firstName, lastName, email, password, phoneNumber } = req.body
 
-  // Case: missing input
   if (!firstName || !lastName || !email || !password || !phoneNumber) {
     return res.status(400).json({
       success: false,
@@ -13,12 +11,23 @@ const register = asyncHandler(async (req, res) => {
     })
   }
 
-  // Case: success
-  const userData = await User.create(req.body)
-  return res.status(200).json({
-    success: !!userData ? true : false,
-    data: userData
-  })
+  const response = await userService.register(req.body)
+
+  return res.status(201).json(response)
 })
 
-export { register }
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: 'Missing inputs'
+    })
+  }
+
+  const response = await userService.login(req.body)
+
+  return res.status(200).json(response)
+})
+
+export { register, login }
--- src/models/user.js
+++ src/models/user.js
@@ -55,10 +55,17 @@ const userSchema = mongoose.Schema({
 })
 
 // eslint-disable-next-line no-unused-vars
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   const saltRounds = 10
   const salt = bcrypt.genSaltSync(saltRounds)
   this.password = await bcrypt.hash(this.password, salt)
 })
+
+// Compare password
+userSchema.methods = {
+  isCorrectPassword: async function(password) {
+    return await bcrypt.compare(password, this.password)
+  }
+}
 
 export default mongoose.model('User', userSchema)
