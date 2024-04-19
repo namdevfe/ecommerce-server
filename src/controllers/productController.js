1892309bf4353676c96f701159928f0479850564
Nguyễn Kim Quốc Nam <namnkq2603@gmail.com>
2024-04-19 11:59:35 +0700

    feat: add filter products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import productService from '~/services/productService'
+import Product from '~/models/product'
 
 const createProduct = asyncHandler(async(req, res) => {
   const hasData = Object.keys(req.body).length > 0
@@ -39,13 +40,53 @@ const updateProduct = asyncHandler(async(req, res) => {
   })
 })
 
+// Filtering, Sorting, Pagination
 const getProducts = asyncHandler(async(req, res) => {
-  const products = await productService.getProducts()
-  return res.status(200).json({
-    success: products ? true : false,
-    message: products ? 'Success' : 'Failed',
-    products
-  })
+  // Build query
+  const queryObj = { ...req.query }
+  const excludedFields = ['page', 'sort', 'limit', 'fields']
+  excludedFields.forEach(key => delete queryObj[key])
+
+  // Format operators correct in MongoDB
+  let queryString = JSON.stringify(queryObj)
+  queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
+  const formatedQueries = JSON.parse(queryString)
+
+  // Filtering
+  if (queryObj?.title) {
+    formatedQueries.title = {
+      $regex: queryObj.title,
+      $options: 'i'
+    }
+  }
+
+  const queryCommand = Product.find(formatedQueries)
+
+  // Execute query
+  // queryCommand.exec(async(error, response) => {
+  //   if (error) throw new Error(error.message)
+  //   const count = await Product.find(formatedQueries).countDocuments()
+  //   return res.status(200).json({
+  //     success: response ? true : false,
+  //     message: response ? 'Success' : 'Failed',
+  //     products: response,
+  //     count
+  //   })
+  // })
+
+  queryCommand
+    .then(async(response) => {
+      const count = await Product.find(formatedQueries).countDocuments()
+      return res.status(200).json({
+        success: response ? true : false,
+        message: response ? 'Success' : 'Failed',
+        products: response,
+        count
+      })
+    })
+    .catch(error => {
+      throw new Error(error)
+    })
 })
 
 const getProductById = asyncHandler(async(req, res) => {
